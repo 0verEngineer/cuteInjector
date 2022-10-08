a03f32410a9a2f71e6427c379b32ca1041904137
0verEngineer <dev@hackinger.io>
2022-10-08 21:58:18 +0200

    Fix disabled menuBar entries in the style palette

--- cuteInjector/src/ui/Style.h
+++ cuteInjector/src/ui/Style.h
@@ -10,26 +10,34 @@ void ApplyStyle(QApplication* app)
     app->setStyle(QStyleFactory::create("Fusion"));
 
     QPalette darkPalette;
-    darkPalette.setColor(QPalette::Window,QColor(53,53,53));
+
+    QColor darkGray(53, 53, 53);
+    QColor darkerGray(42,42,42);
+    QColor gray(127, 127, 127);
+    QColor blue(42, 130, 218);
+
+    darkPalette.setColor(QPalette::Window,darkGray);
     darkPalette.setColor(QPalette::WindowText,Qt::white);
-    darkPalette.setColor(QPalette::Disabled,QPalette::WindowText,QColor(127,127,127));
-    darkPalette.setColor(QPalette::Base,QColor(42,42,42));
+    darkPalette.setColor(QPalette::Base,darkerGray);
     darkPalette.setColor(QPalette::AlternateBase,QColor(66,66,66));
     darkPalette.setColor(QPalette::ToolTipBase,Qt::white);
     darkPalette.setColor(QPalette::ToolTipText,Qt::white);
     darkPalette.setColor(QPalette::Text,Qt::white);
-    darkPalette.setColor(QPalette::Disabled,QPalette::Text,QColor(127,127,127));
     darkPalette.setColor(QPalette::Dark,QColor(35,35,35));
     darkPalette.setColor(QPalette::Shadow,QColor(20,20,20));
-    darkPalette.setColor(QPalette::Button,QColor(53,53,53));
+    darkPalette.setColor(QPalette::Button,darkGray);
     darkPalette.setColor(QPalette::ButtonText,Qt::white);
-    darkPalette.setColor(QPalette::Disabled,QPalette::ButtonText,QColor(127,127,127));
     darkPalette.setColor(QPalette::BrightText,Qt::red);
-    darkPalette.setColor(QPalette::Link,QColor(42,130,218));
-    darkPalette.setColor(QPalette::Highlight,QColor(42,130,218));
-    darkPalette.setColor(QPalette::Disabled,QPalette::Highlight,QColor(80,80,80));
+    darkPalette.setColor(QPalette::Link,blue);
+    darkPalette.setColor(QPalette::Highlight,blue);
     darkPalette.setColor(QPalette::HighlightedText,Qt::white);
-    darkPalette.setColor(QPalette::Disabled,QPalette::HighlightedText,QColor(127,127,127));
+
+    darkPalette.setColor(QPalette::Disabled,QPalette::Highlight,QColor(80,80,80));
+    darkPalette.setColor(QPalette::Disabled,QPalette::HighlightedText,gray);
+    darkPalette.setColor(QPalette::Disabled,QPalette::ButtonText,gray);
+    darkPalette.setColor(QPalette::Disabled,QPalette::Text,gray);
+    darkPalette.setColor(QPalette::Disabled,QPalette::WindowText,gray);
+    darkPalette.setColor(QPalette::Disabled, QPalette::Light, darkGray);
 
     app->setPalette(darkPalette);
 }
